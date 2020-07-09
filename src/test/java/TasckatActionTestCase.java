bf30348d6d120f4c3d12378c0b8b3c0930f57e45
Shahrad Rezaei <shahrad@rezaei.io>
2020-07-09 01:37:20 -0400

    test: add test for taskcat success scenario
    Add a test to verify the success scenario, where taskcat is provided
    with valid AWS credentials and deploys a CloudFormation template.
    
    Because the mock layer has not yet been implemented, we are actually
    hitting AWS to run these tests. The AWS credentials are provided to the
    test via Maven Surefire parameters passed through the `mvn` command from
    the workflow. The credentials themselves are stored as repository
    secrets.
    
    We implement the success scenario ahead of the changes to the Docker
    container to ensure that the action works both before and after we use
    the taskcat/taskcat container instead of the local Dockerfile.
    
    Associated issue: ShahradR/action-taskcat#20

--- src/test/java/TasckatActionTestCase.java
+++ src/test/java/TasckatActionTestCase.java
@@ -35,4 +35,39 @@ public class TasckatActionTestCase {
 
     Assert.assertEquals(1, process.exitValue());
   }
+
+  @Test
+  public void test_when_creds_are_available_then_create_cfn()
+    throws IOException {
+
+    String awsAccessKeyId = System.getProperty("awsAccessKeyId");
+    String awsSecretAccessKey = System.getProperty("awsSecretAccessKey");
+
+    ProcessBuilder pBuilder = new ProcessBuilder(
+      "act",
+      "--job",
+      "taskcat",
+      "--directory",
+      "./src/test/resources/default/",
+      "--secret",
+      "AWS_ACCESS_KEY_ID=" + awsAccessKeyId,
+      "--secret",
+      "AWS_SECRET_ACCESS_KEY=" + awsSecretAccessKey
+    );
+    pBuilder.redirectErrorStream(true);
+    Process process = pBuilder.start();
+
+    String output = new BufferedReader(
+      new InputStreamReader(process.getInputStream())
+    )
+      .lines()
+      .collect(Collectors.joining("\n"));
+
+    assertThat(
+      output,
+      containsString("CREATE_COMPLETE")
+    );
+
+    Assert.assertEquals(0, process.exitValue());
+  }
 }
