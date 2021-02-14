b248de5c566f596389767f9d75fac80e22e963f3
Shahrad Rezaei <shahrad@rezaei.io>
2021-02-14 01:10:57 -0500

    refactor: inject @actions/core using DI container
    Inject the @actions/core module using the InversifyJS DI container.

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -1,10 +1,16 @@
 import "reflect-metadata";
 import { Container } from "inversify";
 import { TYPES } from "./types";
-import { TaskcatArtifactManager, Artifact, PostEntrypoint } from "./interfaces";
+import {
+  TaskcatArtifactManager,
+  Artifact,
+  PostEntrypoint,
+  Core,
+} from "./interfaces";
 import { TaskcatArtifactManagerImpl } from "./taskcat-artifact-manager";
 import { PostEntrypointImpl } from "./post-entrypoint";
 import * as artifact from "@actions/artifact";
+import * as core from "@actions/core";
 
 const prodContainer = new Container();
 
@@ -13,6 +19,7 @@ prodContainer
   .to(TaskcatArtifactManagerImpl);
 
 prodContainer.bind<Artifact>(TYPES.Artifact).toConstantValue(artifact);
+prodContainer.bind<Core>(TYPES.Core).toConstantValue(core);
 
 prodContainer.bind<PostEntrypoint>(TYPES.PostEntrypoint).to(PostEntrypointImpl);
 
