a185d956330916b1a73d19068d973f4aa55396af
Shahrad Rezaei <shahrad@rezaei.io>
2021-02-14 01:35:02 -0500

    refactor: inject child_process using DI container
    Inject the child_process module using the InversifyJS DI container.

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -6,11 +6,13 @@ import {
   Artifact,
   PostEntrypoint,
   Core,
+  ChildProcess,
 } from "./interfaces";
 import { TaskcatArtifactManagerImpl } from "./taskcat-artifact-manager";
 import { PostEntrypointImpl } from "./post-entrypoint";
 import * as artifact from "@actions/artifact";
 import * as core from "@actions/core";
+import * as cp from "child_process";
 
 const prodContainer = new Container();
 
@@ -20,6 +22,8 @@ prodContainer
 
 prodContainer.bind<Artifact>(TYPES.Artifact).toConstantValue(artifact);
 prodContainer.bind<Core>(TYPES.Core).toConstantValue(core);
+
+prodContainer.bind<ChildProcess>(TYPES.ChildProcess).toConstantValue(cp);
 
 prodContainer.bind<PostEntrypoint>(TYPES.PostEntrypoint).to(PostEntrypointImpl);
 
