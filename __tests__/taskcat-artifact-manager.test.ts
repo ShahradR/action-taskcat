d7c6a1ccdf6cbbcad9762e8647d5766016be38af
Shahrad Rezaei <shahrad@rezaei.io>
2021-02-28 01:07:06 -0500

    refactor: refactor TaskcatArtifactManager tests
    Refactor the TaskcatArtifactManager tests based on recent changes.
    
    One major change includes the removal of the InversifyJS containers,
    which best-practice dictates should not be used in unit tests. For more
    details, see:
    
    - How not to do dependency injection, by Paul Hiles
      (https://bit.ly/3sD5dzl)
    
    - Using DI container in unit tests, by Jeff Dege, StriplingWarrior, and
      Steven (https://stackoverflow.com/q/32594803/5875898)

--- __tests__/taskcat-artifact-manager.test.ts
+++ __tests__/taskcat-artifact-manager.test.ts
@@ -1,164 +1,171 @@
-import { TaskcatArtifactManager } from "../src/interfaces";
+import { ArtifactClient } from "../src/interfaces";
 import { prodContainer } from "../src/inversify.config";
-import { TYPES } from "../src/types";
 import { readFileSync, writeFileSync } from "fs";
 import { sync } from "glob";
+import * as replaceInFile from "replace-in-file";
 import { mock } from "jest-mock-extended";
-import * as artifact from "@actions/artifact";
 import * as core from "@actions/core";
-import * as artifactInternal from "@actions/artifact/lib/internal/artifact-client";
+import { TaskcatArtifactManagerImpl } from "../src/taskcat-artifact-manager";
 
 jest.mock("fs");
 jest.mock("glob");
 
-describe("the maskAccountId() function", () => {
-  const taskcatArtifactManager: TaskcatArtifactManager = prodContainer.get<TaskcatArtifactManager>(
-    TYPES.TaskcatArtifactManager
-  );
+describe("the TaskcatArtifactManager class", () => {
+  beforeAll(() => {
+    process.env.GITHUB_WORKSPACE = "/github/workspace";
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  const mockReplaceInFileSync = (replaceInFile.sync as unknown) as jest.SpiedFunction<
+    typeof replaceInFile.sync
+  >;
 
   const mockedReadFileSync = (readFileSync as unknown) as jest.MockedFunction<
     typeof readFileSync
   >;
+
   const mockedWriteFileSync = (writeFileSync as unknown) as jest.MockedFunction<
     typeof writeFileSync
   >;
   const mockedGlobSync = (sync as unknown) as jest.MockedFunction<typeof sync>;
 
-  it("should not modify the `tasckat_output/` files if the AWS account ID has not been printed in the logs", () => {
-    expect.assertions(1);
-    jest.clearAllMocks();
+  const mockedArtifactClient = mock<ArtifactClient>();
 
-    const filePath = "taskcat_outputs/";
-    const fileContents = "abcd1234";
-    const awsAccountId = "1234567890";
+  const taskcatArtifactManager = new TaskcatArtifactManagerImpl(
+    mockedArtifactClient,
+    mockReplaceInFileSync
+  );
 
-    process.env.GITHUB_WORKSPACE = "/github/workspace";
+  describe("the maskAccountId function", () => {
+    it("should not modify the `tasckat_output/` files if the AWS account ID has not been printed in the logs", () => {
+      expect.assertions(1);
 
-    mockedGlobSync.mockReturnValue(["taskcat_outputs/test.txt"]);
-    mockedReadFileSync.mockReturnValue(fileContents);
+      const filePath = "taskcat_outputs/";
+      const fileContents = "abcd1234";
+      const awsAccountId = "1234567890";
 
-    taskcatArtifactManager.maskAccountId(awsAccountId, filePath);
+      mockedGlobSync.mockReturnValue(["taskcat_outputs/test.txt"]);
+      mockedReadFileSync.mockReturnValue(fileContents);
 
-    expect(mockedWriteFileSync).not.toHaveBeenCalled();
-  });
-
-  it("should throw an exception if the AWS account ID has been passed as an empty string", () => {
-    expect.assertions(2);
-    jest.clearAllMocks();
-
-    const filePath = "taskcat_outputs/test.txt";
-    const awsAccountId = "";
-
-    expect(() => {
       taskcatArtifactManager.maskAccountId(awsAccountId, filePath);
-    }).toThrow(Error);
 
-    expect(mockedWriteFileSync).not.toHaveBeenCalled();
-  });
-
-  it("should mask the AWS account ID from the `taskcat_outputs/` files if any references are found in the logs", () => {
-    expect.assertions(1);
-    jest.clearAllMocks();
-
-    const filePath = "taskcat_outputs/test.txt";
-    const fileContents = "abcd1234 1234567890";
-    const awsAccountId = "1234567890";
-
-    mockedGlobSync.mockReturnValue(["taskcat_outputs/test.txt"]);
-    mockedReadFileSync.mockImplementation((path, options?): string | Buffer => {
-      if (path === filePath && options === "utf-8") return fileContents;
-
-      throw new Error();
+      expect(mockedWriteFileSync).not.toHaveBeenCalled();
     });
 
-    taskcatArtifactManager.maskAccountId(awsAccountId, "taskcat_outputs/");
+    it("should throw an exception if the AWS account ID has been passed as an empty string", () => {
+      expect.assertions(2);
 
-    expect(mockedWriteFileSync).toHaveBeenCalledWith(
-      filePath,
-      "abcd1234 ***",
-      "utf-8"
-    );
-  });
-});
+      const filePath = "taskcat_outputs/test.txt";
+      const awsAccountId = "";
 
-describe("the publishTaskcatOutputs function", () => {
-  const taskcatArtifactManager: TaskcatArtifactManager = prodContainer.get<TaskcatArtifactManager>(
-    TYPES.TaskcatArtifactManager
-  );
-  const mockedGlobSync = (sync as unknown) as jest.MockedFunction<typeof sync>;
-  const uploadArtifact = artifact.create();
+      expect(() => {
+        taskcatArtifactManager.maskAccountId(awsAccountId, filePath);
+      }).toThrow(Error);
 
-  it("should retrieve all files from the taskcat_output directory", () => {
-    expect.assertions(1);
-
-    mockedGlobSync.mockReturnValue([
-      "taskcat_outputs/test1.txt",
-      "taskcat_outputs/test2.txt",
-    ]);
-
-    const spy = jest.spyOn(uploadArtifact, "uploadArtifact");
-
-    taskcatArtifactManager.publishTaskcatOutputs("taskcat_outputs/");
-
-    expect(spy).toHaveBeenCalledWith(
-      "taskcat_outputs",
-      ["taskcat_outputs/test1.txt", "taskcat_outputs/test2.txt"],
-      "taskcat_outputs/"
-    );
-  });
-});
-
-describe("the maskAndPublishTaskcatArtifacts function", () => {
-  const taskcatArtifactManager: TaskcatArtifactManager = prodContainer.get<TaskcatArtifactManager>(
-    TYPES.TaskcatArtifactManager
-  );
-
-  const mockedArtifactClient: artifactInternal.ArtifactClient = mock<artifact.ArtifactClient>();
-
-  it("prints a debug message", () => {
-    expect.assertions(1);
-
-    const infoSpy = jest.spyOn(core, "info");
-
-    jest.spyOn(taskcatArtifactManager, "maskAccountId").mockReturnValue();
-    jest
-      .spyOn(taskcatArtifactManager, "publishTaskcatOutputs")
-      .mockReturnValue();
-
-    taskcatArtifactManager.maskAndPublishTaskcatArtifacts("123456789");
-
-    expect(infoSpy).toHaveBeenCalledWith(
-      "Entered the maskAndPublishTaskcatArtifacts function"
-    );
-  });
-
-  it("calls the maskAccountId function", () => {
-    expect.assertions(1);
-
-    const spy = jest
-      .spyOn(taskcatArtifactManager, "maskAccountId")
-      .mockReturnValue();
-
-    taskcatArtifactManager.maskAndPublishTaskcatArtifacts("123456789");
-
-    expect(spy).toHaveBeenCalledWith("123456789", "taskcat_outputs/");
-  });
-
-  it("calls the publishTaskcatOutputs function", () => {
-    expect.assertions(1);
-
-    process.env = Object.assign(process.env, {
-      GITHUB_WORKSPACE: "/github/workspace",
+      expect(mockedWriteFileSync).not.toHaveBeenCalled();
     });
 
-    jest.spyOn(taskcatArtifactManager, "maskAccountId").mockReturnValue();
+    it("should mask the AWS account ID from the `taskcat_outputs/` files if any references are found in the logs", () => {
+      expect.assertions(1);
 
-    const spy = jest
-      .spyOn(taskcatArtifactManager, "publishTaskcatOutputs")
-      .mockReturnValue();
+      const filePath = "taskcat_outputs/test.txt";
+      const fileContents = "abcd1234 1234567890";
+      const awsAccountId = "1234567890";
 
-    taskcatArtifactManager.maskAndPublishTaskcatArtifacts("123456789");
+      mockedGlobSync.mockReturnValue(["taskcat_outputs/test.txt"]);
+      mockedReadFileSync.mockImplementation((path, options?):
+        | string
+        | Buffer => {
+        if (path === filePath && options === "utf-8") return fileContents;
 
-    expect(spy).toHaveBeenCalledWith("/github/workspace/taskcat_outputs/");
+        throw new Error();
+      });
+
+      taskcatArtifactManager.maskAccountId(awsAccountId, "taskcat_outputs/");
+
+      expect(mockedWriteFileSync).toHaveBeenCalledWith(
+        filePath,
+        "abcd1234 ***",
+        "utf-8"
+      );
+    });
+  });
+
+  describe("the publishTaskcatOutputs function", () => {
+    it("should retrieve all files from the taskcat_output directory", () => {
+      expect.assertions(1);
+
+      mockedGlobSync.mockReturnValue([
+        "taskcat_outputs/test1.txt",
+        "taskcat_outputs/test2.txt",
+      ]);
+
+      const mockedArtifactClient = mock<ArtifactClient>();
+
+      const taskcatArtifactManager = new TaskcatArtifactManagerImpl(
+        mockedArtifactClient,
+        replaceInFile.sync
+      );
+
+      taskcatArtifactManager.publishTaskcatOutputs("taskcat_outputs/");
+
+      expect(mockedArtifactClient.uploadArtifact).toHaveBeenCalledWith(
+        "taskcat_outputs",
+        ["taskcat_outputs/test1.txt", "taskcat_outputs/test2.txt"],
+        "taskcat_outputs/"
+      );
+    });
+  });
+
+  describe("the maskAndPublishTaskcatArtifacts function", () => {
+    it("prints a debug message", () => {
+      expect.assertions(1);
+
+      const infoSpy = jest.spyOn(core, "info");
+
+      jest.spyOn(taskcatArtifactManager, "maskAccountId").mockReturnValue();
+      jest
+        .spyOn(taskcatArtifactManager, "publishTaskcatOutputs")
+        .mockReturnValue();
+
+      taskcatArtifactManager.maskAndPublishTaskcatArtifacts("123456789");
+
+      expect(infoSpy).toHaveBeenCalledWith(
+        "Entered the maskAndPublishTaskcatArtifacts function"
+      );
+    });
+
+    it("calls the maskAccountId function", () => {
+      expect.assertions(1);
+
+      const spy = jest
+        .spyOn(taskcatArtifactManager, "maskAccountId")
+        .mockReturnValue();
+
+      taskcatArtifactManager.maskAndPublishTaskcatArtifacts("123456789");
+
+      expect(spy).toHaveBeenCalledWith("123456789", "taskcat_outputs/");
+    });
+
+    it("calls the publishTaskcatOutputs function", () => {
+      expect.assertions(1);
+
+      process.env = Object.assign(process.env, {
+        GITHUB_WORKSPACE: "/github/workspace",
+      });
+
+      jest.spyOn(taskcatArtifactManager, "maskAccountId").mockReturnValue();
+
+      const spy = jest
+        .spyOn(taskcatArtifactManager, "publishTaskcatOutputs")
+        .mockReturnValue();
+
+      taskcatArtifactManager.maskAndPublishTaskcatArtifacts("123456789");
+
+      expect(spy).toHaveBeenCalledWith("/github/workspace/taskcat_outputs/");
+    });
   });
 });
