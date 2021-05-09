320db82e2df878665b11d1f446e252c7bb02d9d5
Shahrad Rezaei <shahrad@rezaei.io>
2021-05-09 18:15:26 +0000

    refactor: use const when defining injections var
    Use const instead of let when defining the `injections` variable in the
    src/utils.ts script, since that variable is never re-assigned. This
    resolves the prefer-const error raised by ESLint.

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import { prodContainer } from "./inversify.config";
 
 function bindDependencies(func, dependencies) {
-  let injections = dependencies.map((dependency) => {
+  const injections = dependencies.map((dependency) => {
     return prodContainer.get(dependency);
   });
   return func.bind(func, ...injections);
