f051666be3f15bd644cea3e4bdf2ac8445c52f6a
Shahrad Rezaei <shahrad@rezaei.io>
2021-07-25 06:20:14 +0000

    test: test taskcat exit code behavior
    Add tests to verify that the PostEntrypointImpl class behaves as
    expected, depending on the taskcat run's exit code.
    
    This is done by leveraging the ChildProcessMock class to mock the exit
    code returned when executing taskcat, without actually calling the
    application.

--- __tests__/post-entrypoint.test.ts
+++ __tests__/post-entrypoint.test.ts
@@ -1,9 +1,16 @@
 import "reflect-metadata";
 import { Artifact, Core, ChildProcess } from "../src/interfaces";
 import * as cp from "child_process";
-import { mock, mockDeep, anyArray, anyObject } from "jest-mock-extended";
+import {
+  mock,
+  mockDeep,
+  anyArray,
+  anyObject,
+  anyFunction,
+} from "jest-mock-extended";
 import { PostEntrypointImpl } from "../src/post-entrypoint";
 import { TaskcatArtifactManagerImpl } from "../src/taskcat-artifact-manager";
+import { ChildProcessMock } from "./mocks/childProcessMock";
 
 describe("the PostEntrypoint class", () => {
   describe("the main function", () => {
@@ -58,12 +65,77 @@ describe("the PostEntrypoint class", () => {
         anyObject()
       );
     });
-    it.todo(
-      "should mark the run as failed if taskcat returns with a non-zero exit code"
-    );
-    it.todo(
-      "should mark the run as successful if taskcat returns with an exit code of 0"
-    );
+
+    it("should mark the run as failed if taskcat returns with a non-zero exit code", () => {
+      expect.assertions(2);
+
+      const cp = new ChildProcessMock(1);
+      jest.spyOn(cp, "on");
+
+      /**
+       * Mock representingthe child_process module. We configure it to
+       * return the ChildProcess object created above when we call the "spawn"
+       * function. This will contain the streams we run our unit tests against.
+       */
+      const childProcessMock = mock<ChildProcess>();
+      childProcessMock.spawn.mockReturnValue(cp);
+
+      /**
+       * Mock the "Core" object, and pass dummy values for the "commands" input
+       * parameter. In this case, a value of "test run" should invoke "taskcat
+       * test run"
+       */
+      const core = mockDeep<Core>();
+      core.getInput.mockReturnValue("test run");
+
+      // Invoke the PostEntrypointImpl.run() function with our mock values.
+      new PostEntrypointImpl(
+        mock<Artifact>(),
+        core,
+        childProcessMock,
+        mock<TaskcatArtifactManagerImpl>()
+      ).run();
+
+      expect(cp.on).toHaveBeenCalledWith("exit", anyFunction());
+      expect(core.setFailed).toHaveBeenCalledWith(
+        "The taskcat test did not complete successfully."
+      );
+    });
+
+    it("should mark the run as successful if taskcat returns with an exit code of 0", () => {
+      expect.assertions(2);
+
+      const cp = new ChildProcessMock(0);
+      jest.spyOn(cp, "on");
+
+      /**
+       * Mock representingthe child_process module. We configure it to
+       * return the ChildProcess object created above when we call the "spawn"
+       * function. This will contain the streams we run our unit tests against.
+       */
+      const childProcessMock = mock<ChildProcess>();
+      childProcessMock.spawn.mockReturnValue(cp);
+
+      /**
+       * Mock the "Core" object, and pass dummy values for the "commands" input
+       * parameter. In this case, a value of "test run" should invoke "taskcat
+       * test run"
+       */
+      const core = mockDeep<Core>();
+      core.getInput.mockReturnValue("test run");
+
+      // Invoke the PostEntrypointImpl.run() function with our mock values.
+      new PostEntrypointImpl(
+        mock<Artifact>(),
+        core,
+        childProcessMock,
+        mock<TaskcatArtifactManagerImpl>()
+      ).run();
+
+      expect(cp.on).toHaveBeenCalledWith("exit", anyFunction());
+      expect(core.setFailed).not.toHaveBeenCalled();
+    });
+
     it.todo("should redirect standard and error outputs to the console");
     it.todo("should upload the taskcat reports as an artifact");
     it.todo("should mask the AWS account ID from the taskcat reports");
